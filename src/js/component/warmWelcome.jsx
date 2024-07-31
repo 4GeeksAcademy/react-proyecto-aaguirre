752f4998358eba887b7be7cacb8d473b96f91a20
aaguirre <alejandra36a@gmail.com>
2024-07-31 10:27:41 +0000

    cambios navbar

---
+++ src/js/component/warmWelcome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WarmWelcome = () => {
+    return (
+        <div>
+            <h1>A warm welcome</h1>
+        </div>
+    )
+}
+
+
+export default WarmWelcome
